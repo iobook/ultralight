8736a47a08e0b5122f394e506f3e358fff04ff2b
Mac User <macuser@Macs-MacBook-Pro.local>
2014-07-17 11:58:50 -0600

    Formatting

--- app.rb
+++ app.rb
@@ -23,37 +23,37 @@ use Rack::Flash
 #Context for all methods
 before do
 
-	if request.path_info.include?("classes") then
+if request.path_info.include?("classes") then
 
-		api_user_key = request.env['HTTP_X_ULTRALIGHT_USER_API_KEY']
-		
-		@user = User.find_by_api_key(api_user_key)
+	api_user_key = request.env['HTTP_X_ULTRALIGHT_USER_API_KEY']
+	
+	@user = User.find_by_api_key(api_user_key)
 
-		unless @user then
-			halt 401, {'Content-Type' => 'text/plain'}, 'Invalid User API Key.'
-		end
-
-		application_id = request.env['HTTP_X_ULTRALIGHT_APPLICATION_ID']
-
-		unless application_id then
-			halt 401, {'Content-Type' => 'text/plain'}, 'No application ID Provided.'
-		end
-
-		@application = @user.applications.find(application_id)
-
-		unless @application then
-			halt 401, {'Content-Type' => 'text/plain'}, 'Invalid application ID.'
-		end
-
-	else #This is not an API request but an App request like signing in or viewing a list of applications.
-
-		@user = User.find(session[:user_id])
-
-		if !@user and !request.path_info.include?("sessions") and !request.path_info.include?("users") then
-			flash[:notice] = "Please login to continue."
-			redirect '/users/signin'
-		end
-
+	unless @user then
+		halt 401, {'Content-Type' => 'text/plain'}, 'Invalid User API Key.'
 	end
+
+	application_id = request.env['HTTP_X_ULTRALIGHT_APPLICATION_ID']
+
+	unless application_id then
+		halt 401, {'Content-Type' => 'text/plain'}, 'No application ID Provided.'
+	end
+
+	@application = @user.applications.find(application_id)
+
+	unless @application then
+		halt 401, {'Content-Type' => 'text/plain'}, 'Invalid application ID.'
+	end
+
+else #This is not an API request but an App request like signing in or viewing a list of applications.
+
+	@user = User.find(session[:user_id])
+
+	if !@user and !request.path_info.include?("sessions") and !request.path_info.include?("users") then
+		flash[:notice] = "Please login to continue."
+		redirect '/users/signin'
+	end
+
+end
 
 end
